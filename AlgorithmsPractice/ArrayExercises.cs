b380ba6100b4436677663a9404e95a1118fe50e2
Dan Dumitriu <dandumitriu33@gmail.com>
2020-08-15 20:34:50 +0300

    Add Find 3rd max exercise

--- AlgorithmsPractice/ArrayExercises.cs
+++ AlgorithmsPractice/ArrayExercises.cs
@@ -21,5 +21,36 @@ namespace AlgorithmsPractice
             }
             return result;
         }
+
+        public static int ThirdMaximumNumber(int[] sourceArray)
+        {
+            int result = 0;
+            int firstMax = 0;
+            int secondMax = 0;
+            int thirdMax = 0;
+
+            foreach (int number in sourceArray)
+            {
+                if (number > firstMax)
+                {
+                    thirdMax = secondMax;
+                    secondMax = firstMax;
+                    firstMax = number;
+                }
+                else if (number > secondMax)
+                {
+                    thirdMax = secondMax;
+                    secondMax = number;
+                }
+                else if (number > thirdMax)
+                {
+                    thirdMax = number;
+                }
+            }
+            result = thirdMax;
+            // printing
+            Console.WriteLine(result);
+            return result;
+        }
     }
 }
--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -7,9 +7,12 @@ namespace AlgorithmsPractice
         static void Main(string[] args)
         {
             // Reverse an array
-            int[] sourceArray = new int[5] { 9, 8, 7, 6, 5 };
-            ArrayExercises.ReverseArray(sourceArray);
-            
+            //int[] sourceArray = new int[5] { 9, 8, 7, 6, 5 }; // expected 5, 6, 7, 8, 9
+            //ArrayExercises.ReverseArray(sourceArray);
+
+            // Find the 3rd maximum number
+            int[] sourceArray = new int[6] { 1, 2, 3, 4, 5, 6 }; // expected 4
+            ArrayExercises.ThirdMaximumNumber(sourceArray);
 
 
         }
