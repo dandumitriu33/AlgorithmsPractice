b6ac594def0cf0400cc0e52566711b08724786f5
Dan Dumitriu <dandumitriu33@gmail.com>
2020-08-15 12:58:41 +0300

    Add one class for sanity test

---
+++ AlgorithmsPractice/Multiply.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsPractice
+{
+    public class Multiply
+    {
+        public static int MultiplyInts(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ AlgorithmsPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlgorithmsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // test
+            Console.WriteLine($"From a class: {Multiply.MultiplyInts(2, 3)}");
+        }
+    }
+}
