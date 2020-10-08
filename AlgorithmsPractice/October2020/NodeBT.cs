19a9a7a314275d50bf1a297f265fb3bda1da867e
Dan Dumitriu <dandumitriu33@gmail.com>
2020-10-08 17:48:02 +0300

    Add Binary Tree Node

---
+++ AlgorithmsPractice/October2020/NodeBT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsPractice.October2020
+{
+    public class NodeBT
+    {
+        public int Value { get; set; }
+        public NodeBT Left { get; set; }
+        public NodeBT Right { get; set; }
+    }
+}
--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -12,28 +12,35 @@ namespace AlgorithmsPractice
     {
         static void Main(string[] args)
         {
-            #region Binary Search Tree - find closest value
-            List<int> numbers = new List<int>() { 50, 30, 20, 40, 70, 60, 80 };
-            BinarySearchTree tree = new BinarySearchTree();
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                tree.Insert(numbers[i]);
-            }
-            tree.InOrder();
-            Console.WriteLine($"Search result: {tree.Search(new NodeBST(20), 70).Value}");
+            #region Binary Tree - branch sums
 
-            List<int> numbers2 = new List<int>() { 10, 5, 15, 2, 5, 13, 22, 1, 14 };
-            BinarySearchTree tree2 = new BinarySearchTree();
-            for (int i = 0; i < numbers2.Count; i++)
-            {
-                tree2.Insert(numbers2[i]);
-                Console.WriteLine($"Added {numbers2[i]}");
-            }
-            int target = 12;
-            var result = tree2.FindClosestRecursive(target);
-            var result2 = tree2.FindClosestIterative(target);
-            Console.WriteLine($"The closest number to {target} is {result}. Recursive solution.");
-            Console.WriteLine($"The closest number to {target} is {result2}. Iterative solution.");
+
+
+
+            #endregion
+
+            #region Binary Search Tree - find closest value
+            //List<int> numbers = new List<int>() { 50, 30, 20, 40, 70, 60, 80 };
+            //BinarySearchTree tree = new BinarySearchTree();
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    tree.Insert(numbers[i]);
+            //}
+            //tree.InOrder();
+            //Console.WriteLine($"Search result: {tree.Search(new NodeBST(20), 70).Value}");
+
+            //List<int> numbers2 = new List<int>() { 10, 5, 15, 2, 5, 13, 22, 1, 14 };
+            //BinarySearchTree tree2 = new BinarySearchTree();
+            //for (int i = 0; i < numbers2.Count; i++)
+            //{
+            //    tree2.Insert(numbers2[i]);
+            //    Console.WriteLine($"Added {numbers2[i]}");
+            //}
+            //int target = 12;
+            //var result = tree2.FindClosestRecursive(target);
+            //var result2 = tree2.FindClosestIterative(target);
+            //Console.WriteLine($"The closest number to {target} is {result}. Recursive solution.");
+            //Console.WriteLine($"The closest number to {target} is {result2}. Iterative solution.");
 
             #endregion
 
